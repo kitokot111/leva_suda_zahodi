9a44e83d746dff5aa114425b3e390635ec331f85
daniel <danilafransev1337@gmail.com>
2024-08-07 15:22:47 +0700

    homework 3

--- lib/main.dart
+++ lib/main.dart
@@ -5,62 +5,124 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.white,
-          leading: IconButton(
-            icon: Icon(Icons.menu),
-            onPressed: () {},
-          ),
           title: Text(
-            'Weather',
+            'Weather Forecast',
             style: TextStyle(
-              color: Colors.black87,
-              fontSize: 25,
+              fontSize: 20,
               fontWeight: FontWeight.bold,
             ),
           ),
           centerTitle: true,
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.settings),
-            ),
-          ],
         ),
-        body: _buildBody(),
+        body:
+        _mainBody(),
       ),
     );
   }
 }
 
-Widget _buildBody() {
-  return SingleChildScrollView(
+Container _mainBody() {
+  List<String> days = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday'
+  ];
+  return Container(
+    padding: EdgeInsets.all(15),
     child: Column(
       children: [
-        _headerImage(),
-        SafeArea(
-          child: Padding(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                _weatherDescription(),
-                Divider(),
-                _temperature(),
-                Divider(),
-                _temperatureForecast(),
-                Divider(),
-                _footerRatings(),
-              ],
+        Row(
+          children: [
+            Icon(Icons.search),
+            SizedBox(
+              width: 15,
             ),
-            padding: EdgeInsets.all(
-              16,
+            Text(
+              'Enter City Name',
             ),
+          ],
+        ),
+        Divider(),
+        SizedBox(
+          height: 20,
+        ),
+        Text(
+          'Tomsk oblast, RU',
+          style: TextStyle(
+            fontSize: 40,
+          ),
+        ),
+        Text('Monday, June 22, 2024'),
+        SizedBox(
+          height: 60,
+        ),
+        _iconsBody(),
+        SizedBox(
+          height: 40,
+        ),
+        _iconsSmallBody(),
+        SizedBox(
+          height: 40,
+        ),
+        _forecast(),
+        SizedBox(
+          height: 5,
+        ),
+        Container(
+          height: 70,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: days.length,
+            itemBuilder: (context, index) {
+              return Container(
+                height: 70,
+                color: Colors.grey.shade400,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      '${days[index]}',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          '${17 + index} °C',
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 6,
+                        ),
+                        Icon(
+                          Icons.sunny,
+                          color: Colors.yellowAccent,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.only(
+                  right: 5,
+                ),
+              );
+            },
           ),
         ),
       ],
@@ -68,124 +130,94 @@ Widget _buildBody() {
   );
 }
 
-Image _headerImage() {
-  return Image(
-    image: NetworkImage(
-        'https://kartinkof.club/uploads/posts/2022-04/1649965770_39-kartinkof-club-p-solntse-kartinki-prikolnie-43.jpg'),
-    fit: BoxFit.cover,
-  );
-}
-
-Column _weatherDescription() {
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.center,
+Row _iconsBody() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
     children: [
-      Text(
-        'Monday - June 11',
-        style: TextStyle(
-          fontSize: 15,
-          fontWeight: FontWeight.bold,
-        ),
+      Icon(
+        Icons.sunny_snowing,
+        color: Colors.amber,
+        size: 70,
       ),
-      Divider(),
-      Text(
-        'Today the weather is great, how is my mood, today I don\'t want to open up, but I want to jump and eat delicious food!',
-        style: TextStyle(
-          color: Colors.black,
-        ),
+      Column(
+        children: [
+          Text(
+            '21 °C',
+            style: TextStyle(
+              fontSize: 40,
+            ),
+          ),
+          Text('IT\'S SUNNY AND RAINY'),
+        ],
       ),
     ],
   );
 }
 
-Row _temperature() {
+Row _iconsSmallBody() {
   return Row(
-    mainAxisAlignment: MainAxisAlignment.center,
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
     children: [
       Column(
         children: [
           Icon(
-            Icons.wb_sunny_sharp,
-            color: Colors.yellow,
+            Icons.air_rounded,
+            color: Colors.blue,
           ),
+          Text(
+            '3',
+            style: TextStyle(
+              fontSize: 17,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text('km/hr'),
         ],
-      ),
-      SizedBox(
-        width: 16,
       ),
       Column(
         children: [
-          Row(
-            children: [
-              Text(
-                '\t 27°C  -\t Clear sky and sunny!',
-              ),
-            ],
+          Icon(
+            Icons.water_drop_rounded,
+            color: Colors.blueAccent,
           ),
-          Row(
-            children: [
-              Text(
-                'Tomsk region, Tomsk city',
-              ),
-            ],
+          Text(
+            '40',
+            style: TextStyle(
+              fontSize: 17,
+              fontWeight: FontWeight.bold,
+            ),
           ),
+          Text('%'),
+        ],
+      ),
+      Column(
+        children: [
+          Icon(
+            Icons.compress_outlined,
+            color: Colors.blueGrey,
+          ),
+          Text(
+            '740',
+            style: TextStyle(
+              fontSize: 17,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text('mm/mec'),
         ],
       ),
     ],
   );
 }
 
-Wrap _temperatureForecast() {
-  return Wrap(
-    spacing: 10,
-    children: List.generate(7, (int index) {
-      return Chip(
-        label: Text(
-          '${index + 20}°C',
-          style: TextStyle(fontSize: 14),
-        ),
-        avatar: Icon(
-          Icons.wb_sunny_outlined,
-          color: Colors.yellow,
-        ),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20),
-          side: BorderSide(
-            color: Colors.black38,
-          ),
-        ),
-        backgroundColor: Colors.white,
-      );
-    }),
-  );
-}
-
-Row _footerRatings() {
-  return Row(
+Column _forecast() {
+  return Column(
     children: [
-      Text('Info from weatherforyou.ru'),
-      SizedBox(
-        width: 60,
-      ),
-      Icon(
-        Icons.star,
-        color: Colors.amber,
-      ),
-      Icon(
-        Icons.star,
-        color: Colors.amber,
-      ),
-      Icon(
-        Icons.star,
-        color: Colors.amber,
-      ),
-      Icon(
-        Icons.star,
-        color: Colors.amber,
-      ),
-      Icon(
-        Icons.star,
-        color: Colors.grey,
+      Text(
+        '7-DAY WEATHER FORECAST',
+        style: TextStyle(
+          fontSize: 15,
+        ),
       ),
     ],
   );
