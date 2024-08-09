3f487b402298d5af485462c8804a70a153ae39fc
daniel <danilafransev1337@gmail.com>
2024-08-09 13:04:32 +0700

    training

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,181 +14,232 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.white,
-          leading: IconButton(
-            icon: Icon(Icons.menu),
-            onPressed: () {},
-          ),
           title: Text(
-            'Weather',
+            'Registration',
             style: TextStyle(
-              color: Colors.black87,
-              fontSize: 25,
               fontWeight: FontWeight.bold,
             ),
           ),
           centerTitle: true,
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.settings),
-            ),
-          ],
         ),
-        body: _buildBody(),
+        body: _registrationBody(),
       ),
     );
   }
 }
 
-Widget _buildBody() {
-  return SingleChildScrollView(
-    child: Column(
-      children: [
-        _headerImage(),
-        SafeArea(
-          child: Padding(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                _weatherDescription(),
-                Divider(),
-                _temperature(),
-                Divider(),
-                _temperatureForecast(),
-                Divider(),
-                _footerRatings(),
+class _registrationBody extends StatefulWidget {
+  const _registrationBody({super.key});
+
+  @override
+  State<_registrationBody> createState() => _registrationBodyState();
+}
+final _formKey = GlobalKey<FormState>();
+bool _hidePass = true;
+final _nameController = TextEditingController();
+final _phoneController = TextEditingController();
+final _emailController = TextEditingController();
+final _storyController = TextEditingController();
+final _passController = TextEditingController();
+final _confirmPassController = TextEditingController();
+
+@override
+void dispose() {
+  _nameController.dispose();
+  _phoneController.dispose();
+  _emailController.dispose();
+  _storyController.dispose();
+  _passController.dispose();
+  _confirmPassController.dispose();
+}
+
+class _registrationBodyState extends State<_registrationBody> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Form(
+        key: _formKey,
+        child: ListView(
+          padding: EdgeInsets.all(16.0),
+          children: [
+            TextFormField(
+              controller: _nameController,
+              decoration: InputDecoration(
+                labelText: 'Full Name *',
+                hintText: 'How people call you?',
+                prefixIcon: Icon(
+                  Icons.person,
+                ),
+                suffixIcon: Icon(
+                  Icons.delete_outline,
+                  color: Colors.red,
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20.0),
+                  ),
+                  borderSide: BorderSide(
+                    color: Colors.blue,
+                    width: 2.0,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20.0),
+                  ),
+                  borderSide: BorderSide(
+                    color: Colors.yellow,
+                    width: 2.0,
+                  ),
+                ),
+              ),
+                validator: _validateName,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              controller: _phoneController,
+              decoration: InputDecoration(
+                prefixIcon: Icon(
+                  Icons.call,
+                ),
+                suffixIcon: Icon(
+                  Icons.delete_outline,
+                  color: Colors.red,
+                ),
+                hintText: 'What your phone number?',
+                labelText: 'Phone Number *',
+                helperText: 'Phone number format: (XXX)XXX-XXXX',
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20.0),
+                  ),
+                  borderSide: BorderSide(
+                    color: Colors.blue,
+                    width: 2.0,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20.0),
+                  ),
+                  borderSide: BorderSide(
+                    color: Colors.yellow,
+                    width: 2.0,
+                  ),
+                ),
+              ),
+              keyboardType: TextInputType.phone,
+              inputFormatters: [
+                FilteringTextInputFormatter.digitsOnly,
               ],
             ),
-            padding: EdgeInsets.all(
-              16,
+            SizedBox(
+              height: 10,
             ),
-          ),
-        ),
-      ],
-    ),
-  );
-}
-
-Image _headerImage() {
-  return Image(
-    image: NetworkImage(
-        'https://kartinkof.club/uploads/posts/2022-04/1649965770_39-kartinkof-club-p-solntse-kartinki-prikolnie-43.jpg'),
-    fit: BoxFit.cover,
-  );
-}
-
-Column _weatherDescription() {
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.center,
-    children: [
-      Text(
-        'Monday - June 11',
-        style: TextStyle(
-          fontSize: 15,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-      Divider(),
-      Text(
-        'Today the weather is great, how is my mood, today I don\'t want to open up, but I want to jump and eat delicious food!',
-        style: TextStyle(
-          color: Colors.black,
-        ),
-      ),
-    ],
-  );
-}
-
-Row _temperature() {
-  return Row(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      Column(
-        children: [
-          Icon(
-            Icons.wb_sunny_sharp,
-            color: Colors.yellow,
-          ),
-        ],
-      ),
-      SizedBox(
-        width: 16,
-      ),
-      Column(
-        children: [
-          Row(
-            children: [
-              Text(
-                '\t 27°C  -\t Clear sky and sunny!',
+            TextFormField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                labelText: 'Email Adress',
+                icon: Icon(
+                  Icons.mail,
+                ),
+                hintText: 'Write your email adress',
               ),
-            ],
-          ),
-          Row(
-            children: [
-              Text(
-                'Tomsk region, Tomsk city',
+              keyboardType: TextInputType.emailAddress,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              controller: _storyController,
+              decoration: InputDecoration(
+                labelText: 'Life Story',
+                icon: Icon(
+                  Icons.nightlife,
+                ),
+                hintText: 'Something about you',
+                border: OutlineInputBorder(),
               ),
-            ],
-          ),
-        ],
+              maxLines: 3,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              controller: _passController,
+              maxLength: 8,
+              obscureText: _hidePass,
+              decoration: InputDecoration(
+                icon: Icon(
+                  Icons.security,
+                ),
+                labelText: 'Password *',
+                hintText: 'Enter your password',
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    setState(() {
+                      _hidePass = !_hidePass;
+                    });
+                  },
+                  icon: Icon(
+                    _hidePass ? Icons.visibility : Icons.visibility_off,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              controller: _confirmPassController,
+              maxLength: 8,
+              obscureText: _hidePass,
+              decoration: InputDecoration(
+                icon: Icon(
+                  Icons.border_color,
+                ),
+                labelText: 'Confirm Password *',
+                hintText: 'Confirm your password',
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            MaterialButton(
+              onPressed:_submitForm,
+              child: Text(
+                'Submit Form',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              color: Colors.blue,
+            ),
+          ],
+        ),
       ),
-    ],
-  );
+    );
+  }
+  void _submitForm() {
+    print('''Name: ${_nameController.text} 
+Phone: ${_phoneController.text} 
+Email: ${_emailController.text} 
+Life story: ${_storyController.text} 
+Password: ${_passController.text}
+Confirm password ${_confirmPassController.text}
+   ''');
+  }
 }
 
-Wrap _temperatureForecast() {
-  return Wrap(
-    spacing: 10,
-    children: List.generate(7, (int index) {
-      return Chip(
-        label: Text(
-          '${index + 20}°C',
-          style: TextStyle(fontSize: 14),
-        ),
-        avatar: Icon(
-          Icons.wb_sunny_outlined,
-          color: Colors.yellow,
-        ),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20),
-          side: BorderSide(
-            color: Colors.black38,
-          ),
-        ),
-        backgroundColor: Colors.white,
-      );
-    }),
-  );
-}
-
-Row _footerRatings() {
-  return Row(
-    children: [
-      Text('Info from weatherforyou.ru'),
-      SizedBox(
-        width: 60,
-      ),
-      Icon(
-        Icons.star,
-        color: Colors.amber,
-      ),
-      Icon(
-        Icons.star,
-        color: Colors.amber,
-      ),
-      Icon(
-        Icons.star,
-        color: Colors.amber,
-      ),
-      Icon(
-        Icons.star,
-        color: Colors.amber,
-      ),
-      Icon(
-        Icons.star,
-        color: Colors.grey,
-      ),
-    ],
-  );
+String? _validateName(String? value) {
+  final _nameExp = RegExp(r'^[A-Za-z ]+$');
+  if (value == null) {
+    return 'Name is reqired.';
+  } else if (!_nameExp.hasMatch(value)) {
+    return 'Please enter alphabetical characters.';
+  } else {
+    return null;
+  }
 }
