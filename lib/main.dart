9efcbc2db4f1e8906e3a39ade8cd20cd5afd9421
daniel <danilafransev1337@gmail.com>
2024-08-11 14:49:55 +0700

    sec

--- lib/main.dart
+++ lib/main.dart
@@ -1,192 +1,96 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _switchOn = false;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          leading: IconButton(
-            icon: Icon(Icons.menu),
-            onPressed: () {},
-          ),
-          title: Text(
-            'Weather',
-            style: TextStyle(
-              color: Colors.black87,
-              fontSize: 25,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          centerTitle: true,
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.settings),
-            ),
-          ],
-        ),
-        body: _buildBody(),
+      home: ChangeNotifierProvider.value(
+        value: Switcher(),
+        child: MyAppBody(),
       ),
     );
   }
 }
 
-Widget _buildBody() {
-  return SingleChildScrollView(
-    child: Column(
-      children: [
-        _headerImage(),
-        SafeArea(
-          child: Padding(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                _weatherDescription(),
-                Divider(),
-                _temperature(),
-                Divider(),
-                _temperatureForecast(),
-                Divider(),
-                _footerRatings(),
-              ],
-            ),
-            padding: EdgeInsets.all(
-              16,
-            ),
+class MyAppBody extends StatefulWidget {
+  const MyAppBody({super.key});
+
+  @override
+  State<MyAppBody> createState() => _MyAppBodyState();
+}
+
+class _MyAppBodyState extends State<MyAppBody> {
+  bool _switchOn = false;
+
+  @override
+  Widget build(BuildContext context) {
+    Switcher anyColor = Provider.of<Switcher>(context);
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'Homework Provider',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: anyColor.containerColor,
           ),
         ),
-      ],
-    ),
-  );
-}
-
-Image _headerImage() {
-  return Image(
-    image: NetworkImage(
-        'https://kartinkof.club/uploads/posts/2022-04/1649965770_39-kartinkof-club-p-solntse-kartinki-prikolnie-43.jpg'),
-    fit: BoxFit.cover,
-  );
-}
-
-Column _weatherDescription() {
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.center,
-    children: [
-      Text(
-        'Monday - June 11',
-        style: TextStyle(
-          fontSize: 15,
-          fontWeight: FontWeight.bold,
-        ),
       ),
-      Divider(),
-      Text(
-        'Today the weather is great, how is my mood, today I don\'t want to open up, but I want to jump and eat delicious food!',
-        style: TextStyle(
-          color: Colors.black,
-        ),
-      ),
-    ],
-  );
-}
-
-Row _temperature() {
-  return Row(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      Column(
-        children: [
-          Icon(
-            Icons.wb_sunny_sharp,
-            color: Colors.yellow,
-          ),
-        ],
-      ),
-      SizedBox(
-        width: 16,
-      ),
-      Column(
-        children: [
-          Row(
-            children: [
-              Text(
-                '\t 27°C  -\t Clear sky and sunny!',
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedContainer(
+              color: anyColor.appBarColor,
+              width: 200,
+              height: 200,
+              duration: Duration(
+                seconds: 1,
               ),
-            ],
-          ),
-          Row(
-            children: [
-              Text(
-                'Tomsk region, Tomsk city',
-              ),
-            ],
-          ),
-        ],
+            ),
+            Switch(
+                value: _switchOn,
+                onChanged: (bool value) {
+                  setState(() {
+                    anyColor.switchedColor();
+                    _switchOn = value;
+                  });
+                }),
+          ],
+        ),
       ),
-    ],
-  );
+    );
+  }
 }
 
-Wrap _temperatureForecast() {
-  return Wrap(
-    spacing: 10,
-    children: List.generate(7, (int index) {
-      return Chip(
-        label: Text(
-          '${index + 20}°C',
-          style: TextStyle(fontSize: 14),
-        ),
-        avatar: Icon(
-          Icons.wb_sunny_outlined,
-          color: Colors.yellow,
-        ),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20),
-          side: BorderSide(
-            color: Colors.black38,
-          ),
-        ),
-        backgroundColor: Colors.white,
-      );
-    }),
-  );
-}
+class Switcher extends ChangeNotifier {
+  Color barColor = Colors.grey;
+  Color conColor = Colors.red;
 
-Row _footerRatings() {
-  return Row(
-    children: [
-      Text('Info from weatherforyou.ru'),
-      SizedBox(
-        width: 60,
-      ),
-      Icon(
-        Icons.star,
-        color: Colors.amber,
-      ),
-      Icon(
-        Icons.star,
-        color: Colors.amber,
-      ),
-      Icon(
-        Icons.star,
-        color: Colors.amber,
-      ),
-      Icon(
-        Icons.star,
-        color: Colors.amber,
-      ),
-      Icon(
-        Icons.star,
-        color: Colors.grey,
-      ),
-    ],
-  );
+  Color get appBarColor => barColor;
+
+  Color get containerColor => conColor;
+
+  void switchedColor() {
+    barColor =
+        Color((Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0);
+    conColor =
+        Color((Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0);
+    notifyListeners();
+  }
 }
